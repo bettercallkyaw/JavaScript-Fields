97588fa3b4069408309a3f94388688d901981a6b
bettercallkyaw <bettercallkyaw@outlook.com>
2022-04-01 10:53:05 +0630

    strings

---
+++ ES6 JS/strings.js
@@ -0,0 +1,8 @@
+const fullName='john doe';
+const age=44;
+
+//concatenation
+console.log('hello my name is '+fullName+'.I am '+age+' year old.');
+
+//template string
+console.log(`hello my name is ${fullName}.I am ${age} year old.`);
