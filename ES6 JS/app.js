fe71c1f5e26b52b80759362cdecdf0af4ccee831
bettercallkyaw <bettercallkyaw@outlook.com>
2022-04-01 10:57:07 +0630

    app

---
+++ ES6 JS/app.js
@@ -0,0 +1,9 @@
+const text='hello world';
+
+console.log(text.substring(0,5).toUpperCase());
+console.log(text.substring(0,5).toLowerCase());
+console.log(text.split(''));
+
+const fruits='apple,banana,orange';
+console.log(fruits.split(','));
+console.log(fruits.split(', '));
